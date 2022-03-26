77aecc0a8aa44d94a2029a5861b9922b889d1dd0
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-03-26 11:28:16 +0500

    add

---
+++ initial/src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+<script>
+export default {
+    name:"Nav"
+}
+</script>
+<style scoped>
+
+</style>
