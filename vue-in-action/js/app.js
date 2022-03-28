5fe000472eab81baac3185cfa8a6d434db29f7b6
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-03-28 15:16:55 +0500

    update vue project

---
+++ vue-in-action/js/app.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        sitename: 'Vue in Actions!',
+        product: {
+            id: 101,
+            title: "Nike Air Max 2022",
+            description: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur recusandae consequuntur id officiis accusantium dolores corrupti. Iusto ea blanditiis enim consectetur animi, alias amet quis unde ut officiis ex qui?",
+            price: 158,
+            image: "./assets/nike.jpg"
+        }
+    }
+})
