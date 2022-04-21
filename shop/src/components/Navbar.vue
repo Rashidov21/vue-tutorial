c53697388b27d817d7ca50b8376f696664421d73
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-04-21 08:59:34 +0500

    add shop vue

--- shop/src/components/Navbar.vue
+++ shop/src/components/Navbar.vue
@@ -2,7 +2,6 @@
  <nav>
  
    <ul>
-
       <li><router-link to="/">Home</router-link> </li>|
       <li><router-link to="/about">About</router-link></li>
    </ul>
--- shop/src/components/Products.vue
+++ shop/src/components/Products.vue
@@ -1,18 +1,37 @@
+
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
-                <a href="#" @click="get">Get</a>
-                <div class="product p-2 m-2">
-                    <img width="200" height="auto" :src="image" class="img-rounded" alt="">
-                    <h2 v-text="title"></h2>
-                    <h3><span v-text="price"></span> </h3>
-                    <div class="mt-2">
-                        <p>
-                            <span v-text="description"></span>
-                        </p>
+            <div class="col-sm-12 col-md-12">
+              
+                <!-- {{products}} -->
+                <div class="row">
+                    <div class="col-sm-6 col-md-4" v-for="item in products" :key="item.id">
+                           
+                        <div class="card row p-2">
+                        
+                            <div class="col-sm-12">
+                                <a href="#">
+                                    <img width="200" height="auto" :src="item.image" class="responsive-img " alt="">
+                                </a>
+                                 <h5><span>{{item.title}}</span> </h5>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="row mt-2">
+                                    <div class="col-6">
+                                        <p class="h3 text-danger">
+                                            <span>${{item.price}}</span>
+                                        </p>
+                                    </div>
+                                   <div class="col-6">
+                                        <a href="#" class="btn btn-primary">Buy</a>
+                                   </div>                                  
+                                </div>
+                            </div>
+                         
+                        
+                        </div>
                     </div>
-
                 </div>
             </div>
 
@@ -21,34 +40,30 @@
 </template>
 <script>
 /* eslint-disable vue/no-unused-components */
+// eslint-disable-next-line vue/require-v-for-key
 export default {
   
     name:"products-component",
     data() {
          
          return {
-             title:"",
-             category:"",
-             price:"",
-             description:"",
-             image:""
-
+             products:null,
+             isActive:true,
          }    
     },
     methods:{
-        get:function(){
-            let res = fetch('https://fakestoreapi.com/products/1')
+
+    },
+    mounted(){
+        fetch('https://fakestoreapi.com/products/')
             .then(res=>res.json())
-            .then(json => {
-                this.title = json.title
-                this.category = json.category
-                this.price = json.price
-                this.description = json.title
-                this.image = json.image
+            .then(data => {
+                this.products = []
+                for(let i of data){
+                    this.products.push(i)
+                }
+                console.log(this.products)
             })
-            
-            return res
-        }
     }
 }
 
