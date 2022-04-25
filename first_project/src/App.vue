5b4b5df8f89436ed5a708cbef73c73248a3ee9a6
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-04-25 16:35:25 +0500

    delete project

--- first_project/src/App.vue
+++ first_project/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
---
+++ first_project/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+      <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">About</a></li>
+      </ul>
+  </nav>
+</template>
+<script>
+export default {
+    name:'navbar-component'
+    
+}
+</script>
+<style scoped>
+nav ul{
+    display: flex;
+    justify-content: flex-end;
+    align-content: center;
+}
+nav ul li{
+    margin: 3px 50px;
+}
+nav ul li a{
+    font-size: 1.8em;
+    color: cadetblue;
+    text-decoration: none;
+}
+</style>
