974af7d8adc21205c02a4208d645947fd9bcc368
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-04-29 17:31:53 +0500

    update project

--- movie/src/views/HomeView.vue
+++ movie/src/views/HomeView.vue
@@ -8,15 +8,17 @@
         Most Popular Movies
       </div>
       <div class="row">
-        <div class="col-sm-6 col-md-3 col-lg-4">
+        <div class="col-sm-6 col-md-3 col-lg-4"
+        v-for="movie , key in movies"
+        v-bind:key="key">
           <div class="movie">
-            <div class="movie-img">
+            <!-- <div class="movie-img">
               <img src="../assets/img/poster.png" alt="">
-            </div>
+            </div> -->
             <div class="movie-info">
-              <div class="title">
-                Batman VS Superman
-              </div>
+              <a v-bind:href="movie.movie_imdb_link" class="title">
+                {{movie.movie_title}}
+              </a>
             </div>
           </div>
         </div>
@@ -27,12 +29,41 @@
 
 <script>
 // @ is an alias to /src
+ /* eslint-disable */
 import Header from '../components/Header.vue'
-
+const axios = require('axios').default;
+import data from '../assets/data.json';
 export default {
   name: 'HomeView',
+  data(){
+    return{
+      movies:[]
+    }
+  },
   components: {
    Header,
+  },
+  mounted(){
+    let list = []
+    for(let k in data){
+        if(list.length > 20){
+          break
+        }
+        list.push(data[k])
+      }
+      this.movies = list
+    // for(let i = 0; i < 20; i++){
+    //    for(let k in data){
+    //       console.log(data[i])
+    //     }
+    // }
+    // axios.get('../assets/data.json',{})
+    // .then(
+    //   data => console.log(data)
+    // )
+    // fetch("../assets/data.json")
+    // .then(response => response.json())
+    // .then(data => console.log(data))
   }
 }
 </script>
