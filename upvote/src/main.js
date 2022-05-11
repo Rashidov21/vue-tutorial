d2d79c8815f6579f6558aacbaefba6cbfbb19529
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-11 14:58:04 +0500

    delete upvote project from repo

--- upvote/src/main.js
+++
@@ -1,8 +0,0 @@
-
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store'
-import 'material-icons/iconfont/material-icons.css';
-require('bulma/css/bulma.min.css');
-
-createApp(App).mount('#app')
--- upvote/src/store/index.js
+++
@@ -1,6 +0,0 @@
-export const store = {
-    state: {
-        number:[1,2,3,4,5]
-    },
-
-}
