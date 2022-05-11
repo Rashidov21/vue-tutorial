4d2acb2d957e6fc893b5125f33079bd228e24f38
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2022-05-11 14:50:59 +0500

    Update DetailView.vue

--- movie/src/views/DetailView.vue
+++ movie/src/views/DetailView.vue
@@ -1,16 +1,43 @@
 <template>
   <div class="detail">
       <section>
-          <div class="container">
+          <div class="container mt-5 ">
               <div class="row">
-                  <div class="col-sm-12 col-md-9">
-                      <div class="card">
+                  <div class="col-sm-12 col-md-12">
+                      <div class="card p-3">
                           <div class="row">
                               <div class="col-sm-12 col-md-6">
                                   <img :src="movie.poster" alt="">
                               </div>
                               <div class="col-sm-12 col-md-6">
-                                  
+                                  <br>
+                                  <h3>Movie detail</h3>
+                                  <ul class="list-group">
+                                      <li class="list-group-item">
+                                          <h5>Title : <b>{{movie.title}}</b></h5>
+                                      </li>
+                                       <li class="list-group-item">
+                                          <h5>Slogan : <b>{{movie.short_description}}</b></h5>
+                                      </li>
+                                      <li class="list-group-item">
+                                          <h5>Country : <b>{{movie.country}}</b></h5>
+                                      </li>
+                                      <li class="list-group-item">
+                                          <h5>Year : <b>{{movie.year}}</b></h5>
+                                      </li>
+                                      <li class="list-group-item">
+                                          <h5>Quality : <b>{{movie.quality}}</b></h5>
+                                      </li>
+                                      <li class="list-group-item">
+                                          <h5>Duration : <b>{{movie.duration}}</b></h5>
+                                      </li>
+                                      <li class="list-group-item">
+                                          <h5>Rating : <b>{{movie.rating}}</b></h5>
+                                      </li>
+                                  </ul>
+                                    <p class="content pt-3">
+                                        {{movie.description}}
+                                    </p>
                               </div>
                           </div>
                       </div>
